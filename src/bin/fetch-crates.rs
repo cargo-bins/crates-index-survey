e18ace2336654fa826eeff62f4872fd2345aaf53
Jiahao XU <Jiahao_XU@outlook.com>
2022-11-25 00:39:34 +1100

    Replace binstall with binstalk-downloader
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/bin/fetch-crates.rs
+++ src/bin/fetch-crates.rs
@@ -6,15 +6,16 @@
 #[cfg(not(tokio_unstable))]
 fn main() {}
 
-use std::{env, fmt::Display};
+use std::{env, fmt::Display, num::NonZeroU64, time::Duration};
 
-use binstall::{helpers::download::Download, manifests::cargo_toml_binstall::PkgFmt};
+use binstalk_downloader::{
+	download::{Download, PkgFmt},
+	remote::Client,
+};
 use crates_index::Index;
 use miette::{IntoDiagnostic, Result};
 use rayon::prelude::ParallelIterator;
-use reqwest::Client;
 use semver::Version;
-use sha2::Sha256;
 #[cfg(tokio_unstable)]
 use tokio::task::JoinSet;
 use tracing::{error, info};
@@ -26,111 +27,114 @@ const CHUNK_SIZE: usize = 512;
 #[cfg(tokio_unstable)]
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(env::var("RUST_LOG").unwrap_or("info".into()))
-        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
-        .init();
+	tracing_subscriber::fmt()
+		.with_env_filter(env::var("RUST_LOG").unwrap_or("info".into()))
+		.with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+		.init();
 
-    info!("Gathering all crates we need to fetch");
-    let all_latests = read_index()?;
-    let total = all_latests.len();
-    info!(%total, "that's a lot of crates");
+	info!("Gathering all crates we need to fetch");
+	let all_latests = read_index()?;
+	let total = all_latests.len();
+	info!(%total, "that's a lot of crates");
 
-    let client = Client::builder()
-        .http2_prior_knowledge()
-        .http2_adaptive_window(true)
-        .build()
-        .into_diagnostic()?;
+	let client = Client::new(
+		concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
+		None,
+		Duration::from_millis(5),
+		NonZeroU64::new(1).unwrap(),
+	)
+	.into_diagnostic()?;
 
-    for (n, chunk) in all_latests.chunks(CHUNK_SIZE).enumerate() {
-        info!("Processing {n}th of {}", all_latests.len() / CHUNK_SIZE + 1);
-        get_a_bunch(client.clone(), n, total, chunk).await?;
-    }
+	for (n, chunk) in all_latests.chunks(CHUNK_SIZE).enumerate() {
+		info!("Processing {n}th of {}", all_latests.len() / CHUNK_SIZE + 1);
+		get_a_bunch(client.clone(), n, total, chunk).await?;
+	}
 
-    Ok(())
+	Ok(())
 }
 
 #[cfg(tokio_unstable)]
 async fn get_a_bunch(client: Client, n: usize, total: usize, chunk: &[IndexCrate]) -> Result<()> {
-    let mut set = JoinSet::new();
+	let mut set = JoinSet::new();
 
-    for (i, crate_version) in chunk.into_iter().enumerate() {
-        set.spawn(
-            crate_version
-                .clone()
-                .download(client.clone(), n * CHUNK_SIZE + i, total),
-        );
-    }
+	for (i, crate_version) in chunk.into_iter().enumerate() {
+		set.spawn(
+			crate_version
+				.clone()
+				.download(client.clone(), n * CHUNK_SIZE + i, total),
+		);
+	}
 
-    while let Some(res) = set.join_next().await {
-        if let Err(err) = res {
-            error!("{err}");
-        }
-    }
+	while let Some(res) = set.join_next().await {
+		if let Err(err) = res {
+			error!("{err}");
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
 
 #[derive(Clone, Debug)]
 struct IndexCrate {
-    pub name: String,
-    pub version: Version,
-    pub url: Url,
-    pub checksum: [u8; 32],
+	pub name: String,
+	pub version: Version,
+	pub url: Url,
+	pub checksum: [u8; 32],
 }
 
 impl IndexCrate {
-    #[tracing::instrument(level = "debug")]
-    async fn download(self, client: Client, i: usize, total: usize) -> Result<()> {
-        info!(target=%self, "downloading {i}/{total}");
-        Download::<Sha256>::new_with_checksum(client, self.url.clone(), self.checksum[..].to_vec())
-            .and_extract(PkgFmt::Tgz, "./crates")
-            .await?;
-        info!(target=%self, "downloaded {i}/{total}");
+	#[tracing::instrument(level = "debug")]
+	async fn download(self, client: Client, i: usize, total: usize) -> Result<()> {
+		info!(target=%self, "downloading {i}/{total}");
+		Download::new(client, self.url.clone())
+			.and_extract(PkgFmt::Tgz, "./crates", None)
+			.await
+			.into_diagnostic()?;
+		info!(target=%self, "downloaded {i}/{total}");
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 impl Display for IndexCrate {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self {
-            name,
-            version,
-            url,
-            checksum,
-        } = self;
-        let ch = u64::from_le_bytes(checksum[0..8].try_into().unwrap());
-        let ec = u64::from_le_bytes(checksum[8..16].try_into().unwrap());
-        let ks = u64::from_le_bytes(checksum[16..24].try_into().unwrap());
-        let um = u64::from_le_bytes(checksum[24..32].try_into().unwrap());
-        write!(f, "{name}@{version}: {url} [{ch:x} {ec:x} {ks:x} {um:x}]")
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let Self {
+			name,
+			version,
+			url,
+			checksum,
+		} = self;
+		let ch = u64::from_le_bytes(checksum[0..8].try_into().unwrap());
+		let ec = u64::from_le_bytes(checksum[8..16].try_into().unwrap());
+		let ks = u64::from_le_bytes(checksum[16..24].try_into().unwrap());
+		let um = u64::from_le_bytes(checksum[24..32].try_into().unwrap());
+		write!(f, "{name}@{version}: {url} [{ch:x} {ec:x} {ks:x} {um:x}]")
+	}
 }
 
 #[tracing::instrument]
 fn read_index() -> Result<Vec<IndexCrate>> {
-    let index = Index::new_cargo_default().into_diagnostic()?;
-    let all_latests: Vec<_> = index
-        .crates_parallel()
-        .filter_map(|c| {
-            c.ok().and_then(|c| {
-                c.highest_normal_version().map(|v| {
-                    let name = v.name().into();
-                    let version = v.version().parse().unwrap();
-                    let url = Url::parse(&format!(
-                        "https://static.crates.io/crates/{name}/{name}-{version}.crate"
-                    ))
-                    .unwrap();
-                    IndexCrate {
-                        name,
-                        version,
-                        url,
-                        checksum: *v.checksum(),
-                    }
-                })
-            })
-        })
-        .collect();
-    Ok(all_latests)
+	let index = Index::new_cargo_default().into_diagnostic()?;
+	let all_latests: Vec<_> = index
+		.crates_parallel()
+		.filter_map(|c| {
+			c.ok().and_then(|c| {
+				c.highest_normal_version().map(|v| {
+					let name = v.name().into();
+					let version = v.version().parse().unwrap();
+					let url = Url::parse(&format!(
+						"https://static.crates.io/crates/{name}/{name}-{version}.crate"
+					))
+					.unwrap();
+					IndexCrate {
+						name,
+						version,
+						url,
+						checksum: *v.checksum(),
+					}
+				})
+			})
+		})
+		.collect();
+	Ok(all_latests)
 }
